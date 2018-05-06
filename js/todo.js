275727a45565c2c40fab0a596e04d14d07f7ffad
Waraphon Roonnapai <eepw0211@gmail.com>
2018-05-06 14:49:59 +0700

    add Create new list code

--- js/todo.js
+++ js/todo.js
@@ -2,13 +2,25 @@ if (jQuery){
 	console.log("jQuery Connect!!");
 }
 
-$("li").click(function(){
+$("ul").on("click","li",function(){
 	$(this).toggleClass("line-thorough")
 });
 
-$("span").click(function(span){
+$("ul").on("click","span",function(span){
 	$(this).parent().fadeOut('500', function() {
 		$(this).remove();
 	});
 	span.stopPropagation();
+});
+
+$("input").keypress(function(event) {
+	if (event.which == 13){
+		var text = this.value ;
+		$(this).val("");
+		$("ul").append("<li><span>X </span>"+ text +"</li>");
+
+	}
+	else{
+		console.log("nope")
+	}
 });
