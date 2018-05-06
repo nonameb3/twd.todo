e841abf613e9bd7cc5a2ce67a6edffe76a8fab0b
Waraphon Roonnapai <eepw0211@gmail.com>
2018-05-06 13:40:42 +0700

    add delete code

--- js/todo.js
+++ js/todo.js
@@ -1,3 +1,14 @@
 if (jQuery){
 	console.log("jQuery Connect!!");
 }
+
+$("li").click(function(){
+	$(this).toggleClass("line-thorough")
+});
+
+$("span").click(function(span){
+	$(this).parent().fadeOut('500', function() {
+		$(this).remove();
+	});
+	span.stopPropagation();
+});
