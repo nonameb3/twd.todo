184b35f7300864f67736f8c66cae852eab2aaa66
Waraphon Roonnapai <eepw0211@gmail.com>
2018-05-06 16:23:18 +0700

    add css code

--- js/todo.js
+++ js/todo.js
@@ -1,26 +1,25 @@
-if (jQuery){
-	console.log("jQuery Connect!!");
+if (jQuery) {
+    console.log("jQuery Connect!!");
 }
 
-$("ul").on("click","li",function(){
-	$(this).toggleClass("line-thorough")
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("line-thorough")
 });
 
-$("ul").on("click","span",function(span){
-	$(this).parent().fadeOut('500', function() {
-		$(this).remove();
-	});
-	span.stopPropagation();
+$("ul").on("click", "span", function(span) {
+    $(this).parent().fadeOut('500', function() {
+        $(this).remove();
+    });
+    span.stopPropagation();
 });
 
 $("input").keypress(function(event) {
-	if (event.which == 13){
-		var text = this.value ;
-		$(this).val("");
-		$("ul").append("<li><span>X </span>"+ text +"</li>");
+    if (event.which == 13) {
+        var text = this.value;
+        $(this).val("");
+        $("ul").append("<li><span>X </span>" + text + "</li>");
 
-	}
-	else{
-		console.log("nope")
-	}
+    } else {
+        console.log("nope")
+    }
 });
